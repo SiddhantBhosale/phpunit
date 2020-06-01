da6bb1cdb1735a92a54f36f9505e46f479bfeb3d
Siddhant S Bhosale <siddhant.bhosale@qed42.com>
2020-06-01 10:02:27 +0530

    commit for testing blocks

---
+++ tests/src/Functional/MyBlockTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\Tests\phpunit\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test the configuration options and block created by Block Example module.
+ *
+ * @group phpunit
+ */
+class MyBlockTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['block', 'block_example'];
+
+  /**
+   * Tests block_example functionality.
+   */
+  public function testBlockExampleBasic() {
+    $assert = $this->assertSession();
+
+    // Create user.
+    $web_user = $this->drupalCreateUser(['administer blocks']);
+    // Login the admin user.
+    $this->drupalLogin($web_user);
+
+    $theme_name = $this->config('system.theme')->get('default');
+
+    // Verify the blocks are listed to be added.
+    $this->drupalGet('/admin/structure/block/library/' . $theme_name, ['query' => ['region' => 'content']]);
+    $assert->pageTextContains('My block');
+
+    // Define and place blocks.
+    $settings_configurable = [
+      'label' => 'Configurable text',
+      'id' => 'block_example_example_configurable_text',
+      'theme' => $theme_name,
+    ];
+    $this->drupalPlaceBlock('example_configurable_text', $settings_configurable);
+
+    $settings_uppercase = [
+      'label' => 'Configurable block to be uppercased',
+      'id' => 'block_example_example_uppercased',
+      'theme' => $theme_name,
+    ];
+    $this->drupalPlaceBlock('example_uppercase', $settings_uppercase);
+
+    $settings_block = [
+      'label' => 'MyBlock block',
+      'id' => 'myblock_demo_test_block',
+      'theme' => $theme_name,
+    ];
+    $this->drupalPlaceBlock('my_block_demo_block', $settings_block);
+
+    // Verify that blocks are there.
+    $this->drupalGet('');
+    $assert->pageTextContains($settings_block['label']);
+  }
+
+}
